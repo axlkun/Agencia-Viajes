d7fad3139c5ef8e18ff5833dd2de02c618b28429
axlkun <axelcruz.dev@gmail.com>
2024-03-04 19:33:07 -0600

    create routes file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import router from './routes/index.js';
 
 const app = express();
 
 const port = process.env.PORT || 4000
+
+// agregar router
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`)
