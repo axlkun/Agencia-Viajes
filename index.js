1994953c24a3bc2065413a13ef00bcb3030a453b
axlkun <axelcruz.dev@gmail.com>
2024-03-04 19:19:09 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+});
