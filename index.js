5a9410d92c34fc9d6c9038865f38f2fc72c40fdd
axlkun <axelcruz.dev@gmail.com>
2024-03-07 19:23:22 -0600

    add dynamic title

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use( (req, res, next) => {
     const year = new Date();
 
     res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = 'Agencia de viajes';
 
     return next();
 });
