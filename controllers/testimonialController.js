c2f718f8e5f7037246cd938349b36219fb704b30
axlkun <axelcruz.dev@gmail.com>
2024-03-15 11:07:52 -0600

    add testimonials to return view with errors

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -25,12 +25,16 @@ const guardarTestimonial = async (req, res) => {
     }
 
     if(errores.length > 0){
+
+        const testimoniales = await Testimonial.findAll();
+
         res.render('testimoniales', {
             pagina: 'Testimoniales',
             errores,
             nombre,
             correo,
-            mensaje
+            mensaje,
+            testimoniales
         })
     }else{
         // Guardar informacion
